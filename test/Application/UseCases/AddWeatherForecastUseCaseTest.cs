30085f5728bae848b1a1d956b04cf9bad3cf70ba
Victor Greggio <vgreggio@yahoo.com.br>
2023-03-15 16:50:36 -0500

    Updating nuget packages.

--- test/Application/UseCases/AddWeatherForecastUseCaseTest.cs
+++ test/Application/UseCases/AddWeatherForecastUseCaseTest.cs
@@ -4,7 +4,7 @@ using AGTec.Common.CQRS.Dispatchers;
 using AGTec.Common.Test;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using Randomizer;
+using AGTec.Common.Randomizer.Impl;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,18 +18,16 @@ namespace uServiceDemo.Application.Test.UseCases
     [TestClass]
     public class AddWeatherForecastUseCaseTest : AutoMockSpecification<AddWeatherForecastUseCase, IAddWeatherForecastUseCase>
     {
-        
+        private Guid _correlationId;
+        private Guid _resultId;
 
-        private Guid correlationId;
-        private Guid resultId;
+        private DateTime _forecastDate;
+        private string _forecastSummary;
+        private int _forecastTemperature;
+        private string _testUsername;
 
-        private DateTime forecastDate;
-        private string forecastSummary;
-        private int forecastTemperature;
-        private string testUsername;
-
-        Mock<ICommandDispatcher> commandDispatcher;
-        Mock<IBackgroundTaskQueue> backgroundTaskQueue;
+        Mock<ICommandDispatcher> _commandDispatcher;
+        Mock<IBackgroundTaskQueue> _backgroundTaskQueue;
 
         protected override void GivenThat()
         {
@@ -38,58 +36,58 @@ namespace uServiceDemo.Application.Test.UseCases
             var randomizerInteger = new RandomIntegerGenerator();
 
             // Sets context CorrelationId
-            correlationId = Guid.NewGuid();
-            CorrelationIdAccessor.CorrelationId = correlationId;
+            _correlationId = Guid.NewGuid();
+            CorrelationIdAccessor.CorrelationId = _correlationId;
 
             // Test data
-            forecastDate = randomizerDate.GenerateValue();
-            forecastSummary = randomizerString.GenerateValue();
-            forecastTemperature = randomizerInteger.GenerateValue();
-            testUsername = randomizerString.GenerateValue();
+            _forecastDate = randomizerDate.GenerateValue();
+            _forecastSummary = randomizerString.GenerateValue();
+            _forecastTemperature = randomizerInteger.GenerateValue();
+            _testUsername = randomizerString.GenerateValue();
 
             // Create AutoMapper instance using Application's configuration
             this.AutoMocker.SetInstance(MapConfig.GetMapperConfiguration().CreateMapper());
 
             // CommandDispatcher Mock
-            commandDispatcher = this.Dep<ICommandDispatcher>();
-            commandDispatcher.Setup(x => x.Execute(It.IsAny<CreateWeatherForecastCommand>()))
+            _commandDispatcher = this.Dep<ICommandDispatcher>();
+            _commandDispatcher.Setup(x => x.Execute(It.IsAny<CreateWeatherForecastCommand>()))
                 .Returns(Task.CompletedTask);
 
             // BackgroundTaskQueue Mock
-            backgroundTaskQueue = this.Dep<IBackgroundTaskQueue>();
-            backgroundTaskQueue.Setup(x => x.Queue(It.IsAny<string>(), It.IsAny<Func<CancellationToken, Task>>()));
+            _backgroundTaskQueue = this.Dep<IBackgroundTaskQueue>();
+            _backgroundTaskQueue.Setup(x => x.Queue(It.IsAny<string>(), It.IsAny<Func<CancellationToken, Task>>()));
         }
 
         protected override void WhenIRun()
         {
-            resultId = this.CreateSut()
-                .Execute(new AddWeatherForecastRequest(){ Date = forecastDate, Summary = forecastSummary, TemperatureInCelsius = forecastTemperature }, testUsername)
+            _resultId = this.CreateSut()
+                .Execute(new AddWeatherForecastRequest(){ Date = _forecastDate, Summary = _forecastSummary, TemperatureInCelsius = _forecastTemperature }, _testUsername)
                 .Result;
         }
 
         [TestMethod]
         public void Should_Return_CorrelationId()
         {
-            Assert.IsTrue(resultId.Equals(correlationId));
+            Assert.IsTrue(_resultId.Equals(_correlationId));
         }
 
         [TestMethod]
         public void Should_Dispatch_Command_With_Properly_Mapped_Entity()
         {
-            commandDispatcher.Verify(x => x.Execute(It.Is<CreateWeatherForecastCommand>(cmd =>
-                cmd.WeatherForecast.Id.Equals(correlationId) &&
-                cmd.WeatherForecast.Summary.Equals(forecastSummary) &&
-                cmd.WeatherForecast.Temperature.Equals(forecastTemperature) &&
-                cmd.WeatherForecast.Date.Equals(forecastDate) &&
-                cmd.Username.Equals(testUsername)
+            _commandDispatcher.Verify(x => x.Execute(It.Is<CreateWeatherForecastCommand>(cmd =>
+                cmd.WeatherForecast.Id.Equals(_correlationId) &&
+                cmd.WeatherForecast.Summary.Equals(_forecastSummary) &&
+                cmd.WeatherForecast.Temperature.Equals(_forecastTemperature) &&
+                cmd.WeatherForecast.Date.Equals(_forecastDate) &&
+                cmd.Username.Equals(_testUsername)
                 )), Times.Once);
         }
 
         [TestMethod]
         public void Should_Queue_Background_Task()
         {
-            var queueMessage = $"Publishing WeatherForecastCreatedEvent for {correlationId}";
-            backgroundTaskQueue.Verify(x => x.Queue(It.Is<String>(msg => msg.Equals(queueMessage)), It.IsAny<Func<CancellationToken, Task>>()), Times.Once);
+            var queueMessage = $"Publishing WeatherForecastCreatedEvent for {_correlationId}";
+            _backgroundTaskQueue.Verify(x => x.Queue(It.Is<String>(msg => msg.Equals(queueMessage)), It.IsAny<Func<CancellationToken, Task>>()), Times.Once);
         }
     }
 }
